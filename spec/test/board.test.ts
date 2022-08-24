6e939759eec8db26cc464301a60ef57a1198758b
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-08-23 22:28:08 -0300

    test: ensure Board throws InvalidEntryError if value is higher than 9

--- spec/test/board.test.ts
+++ spec/test/board.test.ts
@@ -3,9 +3,14 @@ import { Board } from "../../src/game/board"
 
 describe('Board', () => { 
     it('Should throws InvalidEntryError when user inserts a number lower than one', () => {
-
         const input = { value: 0, spot: [1, 2] }
+        const board = new Board()
 
+        expect(() => board.move(input)).toThrow(new InvalidEntryError)
+    })
+
+    it('Should throws InvalidEntryError when user inserts a number lower than one', () => {
+        const input = { value: 10, spot: [1, 2] }
         const board = new Board()
         
         expect(() => board.move(input)).toThrow(new InvalidEntryError)
