503a603c024d6535edf93f1c1f6553367fbe387b
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-08-23 22:26:16 -0300

    feat: ensure Board throws InvalidEntryError if value is lower than 1

---
+++ src/game/board.ts
@@ -0,0 +1,16 @@
+import { InvalidEntryError } from "../errors/invalid-entry-error"
+
+type boardInput = {
+    value: number
+    spot: number[]
+}
+
+export class Board {
+    move = ( input: boardInput ): void => {
+        const { value } = input
+
+        if (value < 1) {
+            throw new InvalidEntryError()
+        }
+    }
+}
