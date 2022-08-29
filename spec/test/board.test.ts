a25a3597190f0237e1c57a838d4a1bb34180867c
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-08-28 22:09:31 -0300

    test: ensure Board returns the current board on success

--- spec/test/board.test.ts
+++ spec/test/board.test.ts
@@ -20,89 +20,116 @@ const getFakeInput = (fakeValue?: number, fakeSpot?: number[]) => ({
 })
 
 describe('Board', () => { 
-    it('Should throws InvalidEntryError when user inserts a number lower than one', () => {
-        const board = makeBoard()
-        const input = getFakeInput(-1)
+    describe('Board.move', () => {
+        it('Should throws InvalidEntryError when user inserts a number lower than one', () => {
+            const board = makeBoard()
+            const input = getFakeInput(-1)
 
-        expect(() => board.move(input)).toThrow(new InvalidEntryError)
+            expect(() => board.move(input)).toThrow(new InvalidEntryError)
+        })
+
+        it('Should throws InvalidEntryError when user inserts a number higher than nine', () => {
+            const board = makeBoard()
+            const input = getFakeInput(10)
+
+            expect(() => board.move(input)).toThrow(new InvalidEntryError)
+        })
+
+        it('Should throws InvalidSpotError when first number in the position is lower than zero', () => {
+            const board = makeBoard()
+            const input = getFakeInput(2, [-1, 2])
+            
+            expect(() => board.move(input)).toThrow(new InvalidSpotError)
+        })
+
+        it('Should throws InvalidSpotError when first number in the position is higher than 8', () => {
+            const board = makeBoard()
+            const input = getFakeInput(2, [9, 2])
+            
+            expect(() => board.move(input)).toThrow(new InvalidSpotError)
+        })
+
+        it('Should throws InvalidSpotError when second number in the position is lower than zero', () => {
+            const board = makeBoard()
+            const input = getFakeInput(2, [1, -1])
+            
+            expect(() => board.move(input)).toThrow(new InvalidSpotError)
+        })
+
+        it('Should throws InvalidSpotError when second number in the position is higher than 8', () => {
+            const board = makeBoard()
+            const input = getFakeInput(2, [1, 9])
+            
+            expect(() => board.move(input)).toThrow(new InvalidSpotError)
+        })
+
+        it('Should throws InvalidSpotError when position has only one value', () => {
+            const board = makeBoard()
+            const input = getFakeInput(2, [1])
+            
+            expect(() => board.move(input)).toThrow(new InvalidSpotError)
+        })
+
+        it('Should throws InvalidSpotError when position has more than 2 values', () => {
+            const board = makeBoard()
+            const input = getFakeInput(2, [1, 2, 3])
+            
+            expect(() => board.move(input)).toThrow(new InvalidSpotError)
+        })
+
+        it('Should throws DuplicatedValueError when the input already exists in a row', () => {
+            const board = makeBoard()
+            const firstInput = getFakeInput(2, [1, 2])
+            const input = getFakeInput(2, [1, 2])
+
+            board.move(firstInput)
+            
+            expect(() => board.move(input)).toThrow(new DuplicatedValueError)
+        })
+
+        it('Should throws DuplicatedValueError when the input already exists in a row', () => {
+            const board = makeBoard()
+            const firstInput = getFakeInput(2, [1, 2])
+            const input = getFakeInput(2, [1, 4])
+
+            board.move(firstInput)
+            
+            expect(() => board.move(input)).toThrow(new DuplicatedValueError)
+        })
+
+        it('Should throws DuplicatedValueError when the input already exists in a column', () => {
+            const board = makeBoard()
+            const firstInput = getFakeInput(2, [5, 7])
+            const input = getFakeInput(2, [1, 7])
+
+            board.move(firstInput)
+            
+            expect(() => board.move(input)).toThrow(new DuplicatedValueError)
+        })
     })
 
-    it('Should throws InvalidEntryError when user inserts a number higher than nine', () => {
-        const board = makeBoard()
-        const input = getFakeInput(10)
+    describe('Board.get', () => {
+        it('Should return the current board on success', () => {
+            const board = makeBoard()
+            const firstInput = getFakeInput(2, [0, 0])
+            const secodnInput = getFakeInput(2, [8, 8])
 
-        expect(() => board.move(input)).toThrow(new InvalidEntryError)
-    })
+            board.move(firstInput)
+            board.move(secodnInput)
 
-    it('Should throws InvalidSpotError when first number in the position is lower than zero', () => {
-        const board = makeBoard()
-        const input = getFakeInput(2, [-1, 2])
-        
-        expect(() => board.move(input)).toThrow(new InvalidSpotError)
-    })
+            const currentBoard = board.get()
 
-    it('Should throws InvalidSpotError when first number in the position is higher than 8', () => {
-        const board = makeBoard()
-        const input = getFakeInput(2, [9, 2])
-        
-        expect(() => board.move(input)).toThrow(new InvalidSpotError)
-    })
-
-    it('Should throws InvalidSpotError when second number in the position is lower than zero', () => {
-        const board = makeBoard()
-        const input = getFakeInput(2, [1, -1])
-        
-        expect(() => board.move(input)).toThrow(new InvalidSpotError)
-    })
-
-    it('Should throws InvalidSpotError when second number in the position is higher than 8', () => {
-        const board = makeBoard()
-        const input = getFakeInput(2, [1, 9])
-        
-        expect(() => board.move(input)).toThrow(new InvalidSpotError)
-    })
-
-    it('Should throws InvalidSpotError when position has only one value', () => {
-        const board = makeBoard()
-        const input = getFakeInput(2, [1])
-        
-        expect(() => board.move(input)).toThrow(new InvalidSpotError)
-    })
-
-    it('Should throws InvalidSpotError when position has more than 2 values', () => {
-        const board = makeBoard()
-        const input = getFakeInput(2, [1, 2, 3])
-        
-        expect(() => board.move(input)).toThrow(new InvalidSpotError)
-    })
-
-    it('Should throws DuplicatedValueError when the input already exists in a row', () => {
-        const board = makeBoard()
-        const firstInput = getFakeInput(2, [1, 2])
-        const input = getFakeInput(2, [1, 2])
-
-        board.move(firstInput)
-        
-        expect(() => board.move(input)).toThrow(new DuplicatedValueError)
-    })
-
-    it('Should throws DuplicatedValueError when the input already exists in a row', () => {
-        const board = makeBoard()
-        const firstInput = getFakeInput(2, [1, 2])
-        const input = getFakeInput(2, [1, 4])
-
-        board.move(firstInput)
-        
-        expect(() => board.move(input)).toThrow(new DuplicatedValueError)
-    })
-
-    it('Should throws DuplicatedValueError when the input already exists in a column', () => {
-        const board = makeBoard()
-        const firstInput = getFakeInput(2, [5, 7])
-        const input = getFakeInput(2, [1, 7])
-
-        board.move(firstInput)
-        
-        expect(() => board.move(input)).toThrow(new DuplicatedValueError)
+            expect(currentBoard).toEqual([
+                [2, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 2],
+            ])
+        })
     })
  })
